55b66d11d96946df3e59ddd46149c3317c212e7e
abdullah-NO <abdullah.hussain97@hotmail.com>
2023-02-22 15:54:10 +0100

    feature-added-05-06-07-SQLFiles

--- Part1/07_powers.sql
+++ Part1/07_powers.sql
@@ -1,6 +1,7 @@
 USE SuperHeroDb;
-INSERT INTO power(Name,PowerId)
+INSERT INTO power(Name,Description)
 VALUES 
-	('Junior Sverre',2),
-    ('Junior Abdullah',1),
-    ('Junior Haavard', 3);
+	('Wind assailant detector','Knows when and who lets out a fart in the vecinity'),
+    ('extra jump','can jump an extra centimeter vertically'),
+    ('the silent picker', 'have the skills and knowledge to pick a bogger without anyone detecting it'),
+	('breath of the wild','instead of fixing bad breath with toothpaste and a brush, it makes it worse');
