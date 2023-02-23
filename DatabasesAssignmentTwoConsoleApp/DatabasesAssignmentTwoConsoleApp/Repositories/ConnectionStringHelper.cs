77cf2ddd5b1388cc41f5aaa43addf8b77570de95
Your Name <sverrevinje@gmail.com>
2023-02-23 13:03:09 +0100

    Console app initialized

---
+++ DatabasesAssignmentTwoConsoleApp/DatabasesAssignmentTwoConsoleApp/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabasesAssignmentTwoConsoleApp.Repositories
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "N-NO-01-01-4564\\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity= true;
+            return connectionStringBuilder.ConnectionString;
+
+        }
+    }
+}
---
+++ DatabasesAssignmentTwoConsoleApp/DatabasesAssignmentTwoConsoleApp/Repositories/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using DatabasesAssignmentTwoConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabasesAssignmentTwoConsoleApp.Repositories
+{
+    public interface ICustomerRepository
+    {
+        public Customer GetCustomer(string id);
+        public List<Customer> GetAllCustomers();
+        public bool AddCustomer(Customer customer);
+        public bool UpdateCustomer(Customer customer);
+        public bool DeleteCustomer(string id);
+
+
+    }
+}
