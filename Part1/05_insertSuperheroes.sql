cb031a37f1ed6806ca6200591d075fd7e31d2367
abdullah-NO <abdullah.hussain97@hotmail.com>
2023-02-26 00:44:59 +0100

    changed some stuff

--- Part1/05_insertSuperheroes.sql
+++ Part1/05_insertSuperheroes.sql
@@ -2,5 +2,5 @@ USE SuperHeroDb;
 INSERT INTO SuperHero(Alias, Name, Origin)
 VALUES 
 	('SuperSverre','Sverre' , 'The gates of Hell'),
-    ('Halludba', 'Abdullah', 'the gates of kebabbiten'),
-    ('your brother', 'Haavard', 'your mom'); 
+    ('Halludba', 'Abdullah', 'Unknown'),
+    ('BrotherFromAMother', 'Haavard', 'mom'); 
